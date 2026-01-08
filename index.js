7f64f3055923d19b4ffa6f48825fbe9890d20a15
zainab dari <Zainab.Dari@student.pxl.be>
2026-01-08 23:11:37 +0100

    git commit -m "Eenvoudige JavaScript-functionaliteit toegevoegd"

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleText() {
+    const tekst = document.getElementById("over-mij");
+
+    if (tekst.style.display === "none") {
+        tekst.style.display = "block";
+    } else {
+        tekst.style.display = "none";
+    }
+}
